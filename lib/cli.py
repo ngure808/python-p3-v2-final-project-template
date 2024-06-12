b43714955867a0b630a062a3ba93b81fcf164d80
ngure808 <mwangingure808@gmail.com>
2024-06-12 11:54:03 +0300

    function that creates an instance of a class and content is fed through the terminal

--- lib/cli.py
+++ lib/cli.py
@@ -1,27 +1,38 @@
 # lib/cli.py
-
-from helpers import (
-    exit_program,
-    helper_1
-)
-
+from models.model_functions import EditorDB
+from helpers import exit_program
 
 def main():
+
+    db = EditorDB()
+
     while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
+        print("\n1. Add Magazine")
+        print("2. Add Author")
+        print("3. Add Article")
+        print("4. Exit")
+        choice = input("Enter Choice: ")
+
+        if choice == "1":
+            main_title = input("Enter Magazine's Main Title: ")
+            genre = input("Enter Magazine's Genre: ")
+            db.add_magazine(main_title,genre)
+
+        elif choice == "2":
+            name = input("Enter Author's Name: ")
+            db.add_author(name)
+
+        elif choice == "3":
+            title = input("Enter Article's Title: ")
+            category = input("Enter Article category: ")
+            db.add_article(title,category)
+
+        elif choice == "4":
             exit_program()
-        elif choice == "1":
-            helper_1()
+            break
+
         else:
-            print("Invalid choice")
-
-
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+            print("Invalid choice. Please try again")
 
 
 if __name__ == "__main__":
